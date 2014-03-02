8a49cb96bc271a1027afc6f210832aa8d3ddd54d
Izaak Meckler <ihmeckler@gmail.com>
2014-03-02 13:57:53 -0500

    updated rotsym

--- rotsym.ml
+++ rotsym.ml
@@ -1,45 +1,9 @@
 open Core
 
-let square w h =
-  let open Draw in let open Point in
-  let side_len = min w h /. 2. in
-  let ctr = {x = w /. 2.; y = h /. 2. } in
-  let square = let open Frp.Behavior in let open Property in
-    rect 
-      ~props:[|return (fill Color.black)|]
-      ~width:(return side_len) ~height:(return side_len)
-      (return {x = (w -. side_len) /. 2.; y = (h -. side_len) /. 2.})
-  in
-  let angle = let open Animate.Sequence in
-       quadratic 1000. ~final:90.
-    >> stay_for 500.
-    >> quadratic 1000. ~final:180.
-    >> stay_for 500.
-    >> quadratic 1000. ~final:270.
-    >> stay_for 500.
-    >> quadratic 1000. ~final:360.
-    >> stay_forever
-    |> run ~init:0.
-    |> Frp.Behavior.map ~f:Angle.of_degrees
-  in
-  transform square (Frp.Behavior.map ~f:(fun a -> Transform.Rotate (a, ctr)) angle)
-
-(*
-let rec range start stop =
-  if stop < start then []
-  else start :: range (start + 1) stop
-
-*)
-
-let pi = 4.0 *. atan 1.0
-
 let range start stop =
   if stop < start then failwith "Invalid range";
   let n = stop - start + 1 in
   Array.init n ~f:(fun i -> i + start)
-
-module Continuous_path = struct
-end
 
 let rotating_ngon w h n =
   let open Draw in
@@ -104,61 +68,34 @@ let rotating_ngon w h n =
 
 let () =
   let svg = Jq.Dom.svg_node "svg" [| "width", "400"; "height", "600" |] in
-  Jq.Dom.append svg (fst (Draw.render (rotating_ngon 400 600 9)));
+  Jq.Dom.append svg (fst (Draw.render (rotating_ngon 400 600 6)));
   match Jq.to_dom_node (Jq.jq "#content") with
     | None -> print "hi"
     | Some t -> Jq.Dom.append t svg
 
 module Continuous_path = struct
+  (* <path stroke="#000000" id="svg_7" d="m74.072388,176.343704c0,0 -48.240629,-187.48112 77.664017,-81.201996c125.904617,106.279099 131.036606,-49.55714 131.036606,-49.55714c0,0 14.027405,-41.795149 -142.669113,-23.882954c-156.696512,17.912197 69.794968,40.60104 69.794968,40.60104c0,0 218.280304,19.106365 -31.818298,157.030354c-250.098579,137.92395 -120.088375,-207.781625 -120.088375,-207.781625" stroke-width="5" fill="none"/>
+  *)
+  let mk () =
+    let container = Jq.jq "#pathanim" in
+    let path_anim slider_val =
+      let open Frp.Behavior in
+      let open Draw in let open Point in let open Segment in
+      path 
+        ~props:[|
+          return Property.(fill Color.({red with alpha = 0.}));
+          return Property.(stroke Color.black 2)
+        |]
+        ~anchor:(return {x = 5.; y = 5.})
+        ~mask:(map slider_val ~f:(fun x -> (0., x ** 2.)))
+        (return [| line_to {x = 100.; y = 100.}; line_to {x = 200.; y = 100.} |])
+(*       |> fill   red *)
+    in
+    let open Widget in
+    create ~width:400 ~height:400 container path_anim
+    +> Control.slider ""
+    |> run
 end
 
-(*
-  let center w h = {Draw.Point. x = float_of_int w /. 2.; y = float_of_int h /. 2.}
+let _ = Continuous_path.mk ()
 
-  let initial_point w h =
-    let radius = min (float_of_int w) (float_of_int h) /. 4. in
-    let c = center w h in
-    Draw.Point.({ c with y = c.y -. radius })
-
-  let ngon w h n =
-    let open Draw in
-    let center_pt, p0 = center w h, initial_point w h in
-    let pts = 
-      let theta  = 2. *. pi /. float_of_int n in
-      Array.map (range 0 (n - 1))
-      ~f:(fun i -> rotate ~about:center_pt p0 (float_of_int i *. theta))
-    in
-    let open Property in let open Frp.Behavior in
-    polygon (return pts) ~props:[|return (fill Color.white); return (stroke Color.black 2)|]
-  ;;
-
-  let rotating_ngon (w : int) (h : int) n =
-    let open Draw in
-    let ngon_shape = ngon w h n in
-    let center_pt, p0 = center w h, initial_point w h in
-    let theta = 360. /. float_of_int n in
-    let angle =
-      let open Animate.Sequence in
-      Array.map (range 1 n)
-        ~f:(fun i -> quadratic 1000. ~final:(float_of_int i *. theta) >> stay_for 500.)
-      |> Array.fold_right ~init:stay_forever ~f:(>>)
-      |> run ~init:0.
-    in
-    let labels = let open Frp.Behavior in
-      let theta = 2. *. pi /. float_of_int n in
-      let pos i = let open Draw.Point in
-        Frp.Behavior.map angle ~f:(fun a -> 
-          rotate ~about:center_pt {p0 with y = p0.y -. 20.} 
-            (degrees_to_radians a +. (float_of_int i *. theta))
-        )
-      in
-      Array.map (range 1 n)
-        ~f:(fun i -> text (return (string_of_int i)) (pos (i - 1)))
-    in
-    pictures (
-      Array.append labels
-        [| transform ngon_shape (Frp.Behavior.map angle ~f:(fun a -> 
-            Transform.Rotate (a, center_pt))) 
-        |]
-    )
-*)
